e3cc91ff891f471e41dcaa7dbb20487a33cc0a8a
Ronell Wright <rwright5github@gmail.com>
2023-03-09 16:44:35 -0500

    added robot cards and search feature

--- src/index.js
+++ src/index.js
@@ -3,13 +3,15 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import 'tachyons';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+// this works with importing 'react-dom/client'
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
